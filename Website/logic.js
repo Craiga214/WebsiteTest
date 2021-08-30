9e5d822d5b361a9135bda49be477ea53ab34cde2
Craiga214 <craiga3164@gmail.com>
2021-08-30 14:47:10 -0400

    Pushing test files for documentation

---
+++ Website/logic.js
@@ -0,0 +1,20 @@
+
+function toHome(){
+    window.location.href = "index.html";
+};
+
+function toAboutMe(){
+    window.location.href = "about_me.html";
+};
+
+function toProjects(){
+    window.location.href = "projects.html";
+};
+
+function toContact(){
+    window.location.href = "contact.html";
+};
+
+function notDone(){
+    alert("This page is under construction.");
+}
